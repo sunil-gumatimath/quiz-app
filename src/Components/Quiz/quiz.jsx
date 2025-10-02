9adbf06c1a592b7463437ed7cc9e457b25f4933e
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-10-02 13:43:42 +0530

    feat(Quiz): Add answer validation and visual feedback
    - Implement lock state to prevent multiple selections after answering
    - Add refs for options to highlight correct answer if wrong choice is made
    - Introduce CSS classes for correct (green) and wrong (red) styling
    - Adjust initial question index for testing purposes

--- src/Components/Quiz/quiz.jsx
+++ src/Components/Quiz/quiz.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
 import { data } from '../../assets/data';
 
 
 const Quiz = () => {
 
-  let [index,setIndex] = useState(4);
-  let [question,setQuestion] = useState(data[index])
+  let [index,setIndex] = useState(2);
+  let [question,setQuestion] = useState(data[index]);
+  let [lock,setLock] = useState(false);
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1,Option2,Option3,Option4];
 
   const checkAns = (e,ans) => {
-    if (question.ans === ans) {
-      e.target.classList.add('correct');
-    }else{
-      e.target.classList.add('wrong');
-    }
+
+    if (lock === false) {
+      if (question.ans === ans) {
+        e.target.classList.add('correct');
+        setLock(true);
+      }else{
+        e.target.classList.add('wrong');
+        setLock(true);
+        option_array[question.ans-1].current.classList.add('correct');
+      }
+    }    
   }
 
   return (
@@ -23,10 +37,10 @@ const Quiz = () => {
       <h2>{index+1}. {question.question}</h2>
       <ul>
         
-        <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-        <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-        <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-        <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+        <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+        <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+        <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+        <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 questions</div>
